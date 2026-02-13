c059242a58ab6508612d6b4944d032222d6f0629
Alan Morelos <amorelos@college.harvard.edu>
2026-02-12 23:24:46 -0500

    fix image?

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
-
 export default function Header() {
   return (
     <div className="flex flex-col items-center gap-2 pb-4 border-b border-gray-800">
-      <Image
-        src="/worldmun-logo.webp"
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/worldmun-qr/worldmun-logo.webp"
         alt="WorldMUN"
         width={72}
         height={72}
-        priority
       />
       <h1 className="text-xl font-semibold text-white tracking-tight">
         Attendance Scanner
